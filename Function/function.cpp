1915d377b2cb45d8f0992d07a5d53430a9a27b1f
Basyrov Konstantin <82223076+jvhab@users.noreply.github.com>
2022-01-27 15:51:00 +0500

    Solution 486A

---
+++ Function/function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	long long n;
+	cin >> n;
+	if (n % 2 == 0)
+		cout << n / 2;
+	else
+		cout << (n / 2) * (-1) - 1;
+	return 0;
+}
