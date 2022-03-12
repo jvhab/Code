155eea63779b7f32a9ddfa4c0ac6ee2b0d8bab1a
Basyrov Konstantin <82223076+jvhab@users.noreply.github.com>
2022-03-12 19:22:52 +0500

    Solution 630A

---
+++ Again_Twenty_Five!/main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	cout << 25;
+	return 0;
+}
