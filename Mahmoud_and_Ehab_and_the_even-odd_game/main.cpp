903dea8388d96653c1bd4b368bf87ade51bcd5a8
Basyrov Konstantin <82223076+jvhab@users.noreply.github.com>
2022-06-02 22:19:45 +0500

    Solution Mahmoud and Ehab and the even odd game

---
+++ Mahmoud_and_Ehab_and_the_even-odd_game/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	if (n % 2 == 0) {
+		cout << "Mahmoud" << "\n";
+	}
+	else {
+		cout << "Ehab" << "\n";
+	}
+	return 0;
+
+}
