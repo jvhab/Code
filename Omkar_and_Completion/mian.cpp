d35024a7d57d9d410341d8fcc41bf54ade4b0e8c
Basyrov Konstantin <82223076+jvhab@users.noreply.github.com>
2022-06-21 22:25:31 +0500

    Solution Omkar and Completion

---
+++ Omkar_and_Completion/mian.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vector<int> v1(n, n);
+		for (auto& i : v1)
+			cout << i << " ";
+		cout << "\n";
+	}
+
+	return 0;
+}
