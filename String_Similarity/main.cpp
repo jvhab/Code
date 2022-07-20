956465948cdae2bcdae7cd6a10966f21d500df52
Basyrov Konstantin <82223076+jvhab@users.noreply.github.com>
2022-07-20 10:46:27 +0500

    Solution String Similarity

---
+++ String_Similarity/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		string s;
+		cin >> s;
+		for (size_t i = 0; i < s.size(); ++i) {
+			if (i % 2 == 0)
+				cout << s[i];
+		}
+		cout << "\n";
+		
+	}
+
+	return 0;
+}
