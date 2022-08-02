b356c26449361ab0750d2bc433c5bef5937e0263
Basyrov Konstantin <82223076+jvhab@users.noreply.github.com>
2022-08-02 21:20:43 +0500

    Solution Strange functions

---
+++ Strange_Functions/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		string s;
+		cin >> s;
+		cout << s.size() << "\n";
+	}
+	return 0;
+}
