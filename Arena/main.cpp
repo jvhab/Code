8dfce4545df55b4022b58af332f4050e390034c8
Basyrov Konstantin <82223076+jvhab@users.noreply.github.com>
2022-08-04 22:48:11 +0500

    Solution Arena

---
+++ Arena/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vector<int> v1(n);
+		for (auto& i : v1)
+			cin >> i;
+		sort(v1.begin(), v1.end());
+		int res{ n };
+		for (size_t i = 0; i < n; ++i) {
+			if (v1[i] == v1[0])
+				res--;
+		}
+		cout << res << "\n";
+	}
+
+	return 0;
+}
