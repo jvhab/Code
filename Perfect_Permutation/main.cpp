38bbfac37ccaa508450abbe083cdda5879758317
Basyrov Konstantin <82223076+jvhab@users.noreply.github.com>
2022-09-06 20:14:44 +0500

    Solution Perfect permutation

--- Perfect_Permutation/main.cpp
+++ Perfect_Permutation/main.cpp
@@ -4,27 +4,23 @@
 #include <algorithm>
 using namespace std;
 
+void solve() {
+    int n;
+    cin >> n;
+    cout << n << " ";
+    for (size_t i = 1; i < n; ++i) {
+        cout << i << " ";
+    }
+    cout << "\n";
+}
+
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	if (n == 1 || n % 2 == 1)
-		cout << -1;
-	else {
-		vector<int> v1(n);
-		int k = 1;
-		for (auto& it : v1) {
-			it = k;
-			k++;
-		}
-		for (size_t i = 0; i < n; i += 2) {
-			swap(v1[i], v1[i + 1]);
-		}
-		for (auto i : v1)
-			cout << i << " ";
-	}
-
-
-	return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
