3bf3c4465c00c42e5ff78fe62dad61476a497365
Basyrov Konstantin <82223076+jvhab@users.noreply.github.com>
2022-11-23 20:21:07 +0500

    Solution Equation

---
+++ Equation/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    long long n;
+    cin >> n;
+    cout << 9 * n << " " << 8 * n << "\n";
+    return 0;
+}
