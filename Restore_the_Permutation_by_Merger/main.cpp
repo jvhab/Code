654c1cfb06a89593cddac7b342482381fddddb70
Basyrov Konstantin <82223076+jvhab@users.noreply.github.com>
2022-12-16 18:36:45 +0500

    Solution Restore the permutation by merger

--- Restore_the_Permutation_by_Merger/main.cpp
+++ Restore_the_Permutation_by_Merger/main.cpp
@@ -2,32 +2,36 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-#include <map>
-#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v1(n * 2);
+    for (auto& i : v1) {
+        cin >> i;
+    }
+    unordered_set<int> st1;
+    vector<int> res;
+    for (size_t i = 0; i < n * 2; i++) {
+        if (st1.find(v1[i]) == st1.end()) {
+            st1.insert(v1[i]);
+            res.push_back(v1[i]);
+        }
+    }
+    for (auto i : res)
+        cout << i << " ";
+    cout << "\n";
+}
+
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int t;
-	cin >> t;
-	while (t--) {
-		int n;
-		cin >> n;
-		vector<int> result;
-		vector<int> temp(2 * n);
-		vector<int> prom(n);
-		for (auto& it : temp)
-			cin >> it;
-		for (size_t i = 0; i < 2 * n; ++i) {
-			if (prom[temp[i] - 1] == 0) {
-				prom[temp[i] - 1] = temp[i];
-				result.push_back(temp[i]);
-			}
-		}
-		for (auto i : result)
-			cout << i << " ";
-		cout << "\n";
-	}
-	return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
