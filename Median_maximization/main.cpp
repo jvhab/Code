a4ba6e5fb352f194ddf5167c9b9610dfd44a4c9b
Basyrov Konstantin <82223076+jvhab@users.noreply.github.com>
2023-01-08 16:14:15 +0500

    Solution Median maximization

---
+++ Median_maximization/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+void solve() {
+	long long n, s;
+	cin >> n >> s;
+	long long m = n / 2 + 1;
+	cout << s / m << "\n";
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
