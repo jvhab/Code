e1aa7095e95d3e18a71425c43c572d1076bbe4a7
Basyrov Konstantin <82223076+jvhab@users.noreply.github.com>
2023-03-08 12:14:15 +0500

    Solution Game

--- Game/main.cpp
+++ Game/main.cpp
@@ -4,33 +4,16 @@
 #include <algorithm>
 using namespace std;
 
-void solve() {
-	string s;
-	cin >> s;
-	int one{}, zero{};
-	for (auto i : s) {
-		if (i == '0') {
-			zero++;
-		}
-		if (i == '1') {
-			one++;
-		}
-	}
-	if (min(zero, one) % 2 == 0) {
-		cout << "NET" << "\n";
-	}
-	else {
-		cout << "DA" << "\n";
-	}
-}
-
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	int t;
-	cin >> t;
-	while (t--) {
-		solve();
+	int n1, n2, k1, k2;
+	cin >> n1 >> n2 >> k1 >> k2;
+	if (n1 > n2) {
+		cout << "First" << "\n";
+	}
+	else {
+		cout << "Second" << "\n";
 	}
 	return 0;
 }
