2b14649ee12d1236cab052b067119867a3bbb7d2
Basyrov Konstantin <82223076+jvhab@users.noreply.github.com>
2023-03-28 19:42:02 +0500

    Solution Splitting into digits

---
+++ Splitting_into_digits/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    cout << n << "\n";
+    for (size_t i = 0; i < n; ++i) {
+        cout << 1 << " ";
+    }
+    return 0;
+}
