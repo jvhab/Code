a94d7fc6814ebd399c0ceb982396b262330a5aba
Basyrov Konstantin <82223076+jvhab@users.noreply.github.com>
2023-03-30 19:58:10 +0500

    Solution Game

--- Game/main.cpp
+++ Game/main.cpp
@@ -4,16 +4,50 @@
 #include <algorithm>
 using namespace std;
 
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v1(n);
+    bool flag{ false };
+    for (auto& i : v1) {
+        cin >> i;
+        if (i == 0) {
+            flag = true;
+        }
+    }
+    if (!flag) {
+        cout << 0 << "\n";
+        return;
+    }
+    int res = n - 1;
+    int tmp{ 0 };
+    for (int i = n - 1; i >= 0; --i) {
+        if (v1[i] == 1) {
+            tmp = i;
+        }
+        else if (v1[i] == 0) {
+            break;
+        }
+    }
+    int tmp2{};
+    for (int i = 0; i < n; ++i) {
+        if (v1[i] == 1) {
+            tmp2 = i;
+        }
+        else if (v1[i] == 0) {
+            ;
+            break;
+        }
+    }
+    cout << tmp - tmp2 << "\n";
+}
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int n1, n2, k1, k2;
-	cin >> n1 >> n2 >> k1 >> k2;
-	if (n1 > n2) {
-		cout << "First" << "\n";
-	}
-	else {
-		cout << "Second" << "\n";
-	}
-	return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
